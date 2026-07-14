2683070f1e63eb210370cd02f85659a48f25defc
Davinga Balya Rochmadi <balyarochmadie@gmail.com>
2026-07-14 11:28:52 +0700

    Use object-tree editor workspace

--- src/batikcraft_studio/ui/views.py
+++ src/batikcraft_studio/ui/views.py
@@ -10,7 +10,7 @@ from batikcraft_studio.application import ProjectSession
 from batikcraft_studio.config import WorkspaceDefinition
 
 from .icons import create_icon
-from .motif_batik_editor import MotifBatikEditorWorkspaceView
+from .object_tree_editor import ObjectTreeEditorWorkspaceView
 from .theme import COLORS
 
 StatusCallback = Callable[[str], None]
@@ -77,9 +77,9 @@ def create_workspace_view(
     set_status: StatusCallback,
     session: ProjectSession,
     refresh_context: RefreshCallback,
-) -> WorkspaceView | MotifBatikEditorWorkspaceView:
+) -> WorkspaceView | ObjectTreeEditorWorkspaceView:
     if definition.key == "editor":
-        return MotifBatikEditorWorkspaceView(
+        return ObjectTreeEditorWorkspaceView(
             parent,
             definition=definition,
             set_status=set_status,
