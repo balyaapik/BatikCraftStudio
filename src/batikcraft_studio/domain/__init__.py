200e6a13e420d444611ec165652e4630b6c1d40e
Davinga Balya Rochmadi <balyarochmadie@gmail.com>
2026-07-14 11:16:12 +0700

    Export object-tree domain types

--- src/batikcraft_studio/domain/__init__.py
+++ src/batikcraft_studio/domain/__init__.py
@@ -8,10 +8,15 @@ from batikcraft_studio.domain.errors import (
 )
 from batikcraft_studio.domain.models import (
     CURRENT_SCHEMA_VERSION,
+    LEGACY_SCHEMA_VERSIONS,
     MAX_CANVAS_DIMENSION,
     CanvasSpec,
     Layer,
     LayerKind,
+    LayerNodeKind,
+    LayerObject,
+    ObjectBounds,
+    ObjectKind,
     ProjectMetadata,
     Transform,
 )
@@ -19,12 +24,17 @@ from batikcraft_studio.domain.project import Project
 
 __all__ = [
     "CURRENT_SCHEMA_VERSION",
+    "LEGACY_SCHEMA_VERSIONS",
     "MAX_CANVAS_DIMENSION",
     "CanvasSpec",
     "DuplicateLayerError",
     "Layer",
     "LayerKind",
+    "LayerNodeKind",
     "LayerNotFoundError",
+    "LayerObject",
+    "ObjectBounds",
+    "ObjectKind",
     "Project",
     "ProjectDomainError",
     "ProjectMetadata",
